570e4260a52e558717de2e78a743c24eecf6e714
Mohamed IDBRAHIM <idbrahimdev@gmail.com>
2019-03-04 09:56:45 +0100

    Updates

---
+++ precache-manifest.05f1d8a259f707310fc0e58687f5af00.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/reactjs-contacts/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "dd53687943993cfe316c",
+    "url": "/reactjs-contacts/static/css/main.ae0dc638.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/reactjs-contacts/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "18045a84209655c08862",
+    "url": "/reactjs-contacts/static/js/1.18045a84.chunk.js"
+  },
+  {
+    "revision": "a34d1ac9502e370e1174",
+    "url": "/reactjs-contacts/static/js/runtime~main.a34d1ac9.js"
+  },
+  {
+    "revision": "dd53687943993cfe316c",
+    "url": "/reactjs-contacts/static/js/main.dd536879.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/reactjs-contacts/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/reactjs-contacts/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/reactjs-contacts/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "18045a84209655c08862",
+    "url": "/reactjs-contacts/static/css/1.921c6252.chunk.css"
+  },
+  {
+    "revision": "9881086cc0a87a4866b142195d28a395",
+    "url": "/reactjs-contacts/index.html"
+  }
+];
